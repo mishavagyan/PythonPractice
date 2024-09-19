bc88689678a64a34fb8eda1ba9ea1ca75aff6eee
Mekhak Avagian <avagyanmexak@gmail.com>
2024-09-19 17:47:31 +0400

    Snake game in console

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 import os
 import time
 import random
+from pynput.keyboard import Key, Listener
 
 matrix_size = 13
 
@@ -8,6 +9,8 @@ matrix = []
 pos_y = [6]
 pos_x = [6]
 eat_pos = [random.randint(1, matrix_size - 2), random.randint(1, matrix_size - 2)]
+key_pressed = None
+
 
 # Make matrix
 for i in range(matrix_size):
@@ -20,47 +23,56 @@ for i in range(matrix_size):
         else:
             matrix[i].append(" ")
 
-
 # function for printin matrix
 def pr():
     os.system("clear")
+    matrix[eat_pos[1]][eat_pos[0]] = "*"
     for i in range(len(pos_y)):
         matrix[pos_y[i]][pos_x[i]] = "#"
-    matrix[eat_pos[1]][eat_pos[0]] = "*"
     for i in matrix:
         for j in i:
             print(j, " ", sep="", end="")
         print()
-    print(eat_pos)
-    time.sleep(0.07)
-    
+    print("Score:", len(pos_x) - 1)
+
 def move_up():
     matrix[pos_y[0]][pos_x[0]] = " "
-    pos_y.append(pos_y[-1] - 1)
+    new_pos_y = pos_y[-1] - 1
+    if new_pos_y == 0:
+        new_pos_y = matrix_size - 2 
+    pos_y.append(new_pos_y)
     pos_y.pop(0)
     pos_x.append(pos_x[-1])
     pos_x.pop(0)
 
-
 def move_right():
     matrix[pos_y[0]][pos_x[0]] = " "
+    new_pos_x = pos_x[-1] + 1
+    if new_pos_x == matrix_size - 1:
+        new_pos_x = 1
     pos_y.append(pos_y[-1])
     pos_y.pop(0)
-    pos_x.append(pos_x[-1] + 1)
+    pos_x.append(new_pos_x)
     pos_x.pop(0)
 
 def move_down():
     matrix[pos_y[0]][pos_x[0]] = " "
-    pos_y.append(pos_y[-1] + 1)
+    new_pos_y = pos_y[-1] + 1
+    if new_pos_y == matrix_size - 1:
+        new_pos_y = 1
+    pos_y.append(new_pos_y)
     pos_y.pop(0)
     pos_x.append(pos_x[-1])
     pos_x.pop(0)
 
 def move_left():
     matrix[pos_y[0]][pos_x[0]] = " "
+    new_pos_x = pos_x[-1] - 1
+    if new_pos_x == 0:
+        new_pos_x = matrix_size - 2
     pos_y.append(pos_y[-1])
     pos_y.pop(0)
-    pos_x.append(pos_x[-1] - 1)
+    pos_x.append(new_pos_x)
     pos_x.pop(0)
 
 def eat():
@@ -69,35 +81,50 @@ def eat():
     global eat_pos
     eat_pos = [random.randint(1, matrix_size - 2), random.randint(1, matrix_size - 2)]
 
-# pr()
-arr_x = []
-arr_y = []
+def die():
+    if (pos_y[-1], pos_x[-1]) in zip(pos_y[:-1], pos_x[:-1]):
+        return True
+    return False
+
+def move_snake():
+    if key_pressed == Key.up:
+        move_up()
+    elif key_pressed == Key.down:
+        move_down()
+    elif key_pressed == Key.right:
+        move_right()
+    elif key_pressed == Key.left:
+        move_left()
+
+def on_press(key):
+    global key_pressed
+    if key == Key.up and key_pressed != Key.down:
+        key_pressed = key
+    elif key == Key.down and key_pressed != Key.up:
+        key_pressed = key
+    elif key == Key.right and key_pressed != Key.left:
+        key_pressed = key
+    elif key == Key.left and key_pressed != Key.right:
+        key_pressed = key
+    
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+listener = Listener(on_press=on_press)
+listener.start()
 
 while True:
-    while pos_y[-1] > 1:
-        move_up()
-        if pos_y[-1] == eat_pos[1] and pos_x[-1] == eat_pos[0]:
-            eat()
-            # eat_pos = [random.randint(1, matrix_size - 1), random.randint(1, matrix_size - 1)]
-        pr()
+    move_snake()
+    if die():
+        print()
+        print("*" * 61)
+        print("*" * 25, " Game Over ", "*" * 25, sep="")
+        print("*" * 61)
+        break
+    if pos_y[-1] == eat_pos[1] and pos_x[-1] == eat_pos[0]:
+        eat()
+    pr()
+    time.sleep(0.2)
 
-    while pos_x[-1] > 1:
-        move_left()
-        if pos_y[-1] == eat_pos[1] and pos_x[-1] == eat_pos[0]:
-            eat()
-            # eat_pos = [random.randint(1, matrix_size - 1), random.randint(1, matrix_size - 1)]
-        pr()
-
-    while pos_y[-1] < matrix_size - 2:
-        move_down()
-        if pos_y[-1] == eat_pos[1] and pos_x[-1] == eat_pos[0]:
-            eat()
-            # eat_pos = [random.randint(1, matrix_size - 1), random.randint(1, matrix_size - 1)]
-        pr()
-
-    while pos_x[-1] < matrix_size - 2:
-        move_right()
-        if pos_y[-1] == eat_pos[1] and pos_x[-1] == eat_pos[0]:
-            eat()
-            # eat_pos = [random.randint(1, matrix_size - 1), random.randint(1, matrix_size - 1)]
-        pr()
+listener.join()
